5f86f950ff19f7acf844d8859f13bfc7681a1576
Salman Zindani <salmankarimzindani@hotmail.com>
2023-10-27 13:41:17 -0500

    feat(jokeService): adds worker service template to project

---
+++ JokeService/Program.cs
@@ -0,0 +1,10 @@
+using JokeService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
